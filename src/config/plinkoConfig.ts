6a753c2aac7d87d5efe55e5c9060f212379db6b3
nigilen <you@example.com>
2026-02-12 10:41:33 +0300

    fix: config files

--- src/config/plinkoConfig.ts
+++ src/config/plinkoConfig.ts
@@ -1,6 +1,49 @@
-export const plinkoConfig = {
+interface PlinkoConfig {
+  scene: {
+    logicalWidth: number;
+    logicalHeight: number;
+  };
+  topCell: {
+    width: number;
+    height: number;
+    position: { x: number; y: number };
+  };
+  ball: {
+    radius: number;
+    startPosition: { x: number; y: number };
+  };
+  pin: {
+    radius: number;
+    position: { x: number; y: number };
+  };
+  bottomCell: {
+    width: number;
+    height: number;
+    position: { x: number; y: number };
+  };
+};
+
+export const plinkoConfig: PlinkoConfig = {
   scene: {
     logicalWidth: 400,
     logicalHeight: 250
-  }
+  },
+  topCell: {
+    width: 100,
+    height: 100,
+    position: { x: 100, y: 0 }
+  },
+  ball: {
+    radius: 10,
+    startPosition: { x: 100, y: 0 }
+  },
+  pin: {
+    radius: 10, 
+    position: { x: 100, y: 0 }
+  },
+  bottomCell: {
+    width: 100,
+    height: 100,
+    position: { x: 100, y: 0 }
+  },
 } as const;
