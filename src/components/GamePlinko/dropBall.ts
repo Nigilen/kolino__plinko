f955ecea1e8808873527cae364d63c5bf58438f3
nigilen <you@example.com>
2026-02-19 15:52:02 +0300

    [lab]: played with physics and logic

--- src/components/GamePlinko/dropBall.ts
+++ src/components/GamePlinko/dropBall.ts
@@ -19,7 +19,6 @@ const config: BallConfig = {
 export const dropBall = (app: Application | null, ball: Container) => {
   if (!app || !ball) return;
 
-  const boxConfig = app.stage.getLocalBounds();
 
 
   interface Border {
@@ -28,101 +27,55 @@ export const dropBall = (app: Application | null, ball: Container) => {
     direction: -1 | 1;
   };
 
-  interface Borders {
-    bottom: Border;
-    right: Border;
-    left: Border;
-  };
-
-  const borders: Borders = {
-    bottom: { axis: "y", value: 234, direction: 1 },
-    right: { axis: "x", value: 125-6, direction: 1 },
-    left: { axis: "x", value: -125+6, direction: -1 },
-  };
+  const borders: Border[] = [
+    { axis: "y", value: 234, direction: 1 },
+    { axis: "x", value: 125-6, direction: 1 },
+    { axis: "x", value: -125+6, direction: -1 },
+  ];
 
   const checkCollision = (ball: Container, border: Border) => {
     const currentPos = border.axis === "x" ? ball.position.x : ball.position.y;
 
-    // if (border.direction === 1) {
-    //   return currentPos > border.value;
-    // } else {
-    //   return currentPos < border.value;
-    // }
-    return (currentPos - border.value) * border.direction > 0;
+    if (border.direction === 1) {
+      return currentPos > border.value;
+    } else {
+      return currentPos < border.value;
+    }
 
-    // —— старый подход
-    // if (border.type === "bottom") {
-    //   return ball.position.y > border.value;
-    // } 
-    // else if (border.type === "right") {
-    //   return ball.position.x > border.value;
-    // }
-    // else if (border.type === "left") {
-    //   return ball.position.x < border.value;
-    // }
   };
 
   const resolveCollision = (ball: Container, border: Border) => {
-    // —— если пересеченная граница вертикальная — равняем позицию и отражаем по вертикали
     if (border.axis === "y") { 
       ball.position.y = border.value;
       config.velocity.y = -config.velocity.y * config.bounce;
     } 
-    // —— если пересеченная граница горизонтальная — равняем позицию и отражаем по горизонтали
     else if (border.axis === "x") { 
       ball.position.x = border.value;
       config.velocity.x = -config.velocity.x * config.bounce;
     }
   };
 
-  const animation = (delta: Ticker) => {
-    config.velocity.y += config.gravity * delta.deltaTime;
-    config.velocity.x *= config.friction * delta.deltaTime;
+  const animation = (ticker: Ticker) => {
+    
+    config.velocity.y += config.gravity * ticker.deltaTime;
+    config.velocity.x *= config.friction;
 
-    ball.position.y += config.velocity.y * delta.deltaTime;
-    ball.position.x += config.velocity.x * delta.deltaTime;
+    ball.position.y += config.velocity.y * ticker.deltaTime;
+    ball.position.x += config.velocity.x * ticker.deltaTime;
 
-    Object.values(borders).forEach((border) => {             // —— перебираем каждую границу
-      if (checkCollision(ball, border)) {     // —— произошло ее пересечение???
-        resolveCollision(ball, border);       // —— отправляем в обработку!
-
-        // —— если скорость меньше гравитации — обнуляемся
-        if (Math.abs(config.velocity.y) < config.gravity) {
-          app.ticker.remove(animation);
-          ball.position.y = 0;
-          ball.position.x = 0;
-          config.velocity.x = 4;
-          config.velocity.y = 0;
-        }
+    borders.forEach((border) => {
+      if (checkCollision(ball, border)) {
+        resolveCollision(ball, border);  
       }
     });
 
-
-    // —— старый подход - хранение данных в условиях
-
-    // // —— пол - ось (y) и значение (234)
-    // if (ball.position.y > boxConfig.maxY - 66) { 
-    //   ball.position.y = boxConfig.maxY - 66;
-    //   config.velocity.y = -config.velocity.y * config.bounce * delta.deltaTime;
-      
-    //   if (Math.abs(config.velocity.y) < config.gravity) {
-    //     app.ticker.remove(animation);
-    //     ball.position.y = 0;
-    //     ball.position.x = 0;
-    //     config.velocity.x = 4;
-    //     config.velocity.y = 0;
-    //   }
-
-    //   // —— правая стена - ось (x) и значение (125-6)
-    // } else if (ball.position.x > boxConfig.maxX - plinkoConfig.ball.radius * 2) { 
-    //   ball.position.x = boxConfig.maxX - plinkoConfig.ball.radius * 2;
-    //   config.velocity.x = -config.velocity.x * delta.deltaTime;
-
-    //   // —— левая стена - ось (x) и значение (125+6)
-    // } else if (ball.position.x < -boxConfig.maxX + plinkoConfig.ball.radius * 2) { 
-    //   ball.position.x = -boxConfig.maxX + plinkoConfig.ball.radius * 2;
-    //   config.velocity.x = -config.velocity.x * delta.deltaTime;
-    // };
+    if (Math.abs(config.velocity.y) < config.gravity && Math.abs(config.velocity.x) < config.gravity) {
+      app.ticker.remove(animation);
+      ball.position.y = 0;
+      ball.position.x = 0;
+      config.velocity.x = 4;
+      config.velocity.y = 0;
+    }
   };
 
   app.ticker.add(animation);
