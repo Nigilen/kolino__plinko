89611cbd81ec89e4d6bbbe131d4b502293d7b891
nigilen <you@example.com>
2026-02-26 01:04:13 +0300

    [fix]: comment onBallCought code

--- src/components/GamePlinko/utils/onBallCaught.ts
+++ src/components/GamePlinko/utils/onBallCaught.ts
@@ -1,14 +1,14 @@
-import type { Container } from "pixi.js";
-import type { Config } from "../types";
+// import type { Container } from "pixi.js";
+// import type { Config } from "../types";
 
-export const onBallCaught = (
-  bounds: { minX: number; maxX: number; minY: number; maxY: number },
-  wallThickness: number,
-  config: Config
-) => {
-  config.obstacles.push(
-    { type: "wall", axis: "x", value: bounds.minX + wallThickness, direction: -1, bounce: 0.2 },
-    { type: "wall", axis: "x", value: bounds.maxX - wallThickness, direction: 1, bounce: 0.2 },
-    { type: "wall", axis: "y", value: bounds.maxY - 15, direction: 1, bounce: 0.4 }
-  );
-};
+// export const onBallCaught = (
+//   bounds: { minX: number; maxX: number; minY: number; maxY: number },
+//   wallThickness: number,
+//   config: Config
+// ) => {
+//   config.obstacles.push(
+//     { type: "wall", axis: "x", value: bounds.minX + wallThickness, direction: -1, bounce: 0.2 },
+//     { type: "wall", axis: "x", value: bounds.maxX - wallThickness, direction: 1, bounce: 0.2 },
+//     { type: "wall", axis: "y", value: bounds.maxY - 15, direction: 1, bounce: 0.4 }
+//   );
+// };
