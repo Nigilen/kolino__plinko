80d3ef076cf192c7e438c828cd3d6c3e244d7a7c
nigilen <you@example.com>
2026-02-27 00:18:02 +0300

    [fix]: cells mob resolution

--- src/components/GamePlinko/assets.ts
+++ src/components/GamePlinko/assets.ts
@@ -3,5 +3,5 @@ import skullBottom from '@/assets/images/cell-skull--bottom.svg';
 
 export const assets = [
   { alias: 'skull', src: skull, data: { resolution: window.devicePixelRatio || 2, scaleMode: 'linear' } },
-  { alias: 'skullBottom', src: skullBottom },
+  { alias: 'skullBottom', src: skullBottom, data: { resolution: window.devicePixelRatio || 2, scaleMode: 'linear' } },
 ];
